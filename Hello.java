c6f98aaf4e7e364a8461363f28bd9e9c5a6925de
Hemantkumar <88929656+Hemant-1111@users.noreply.github.com>
2024-10-18 12:25:49 +0545

    Update Hello.java
    second feature insertion

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello
     {
         System.out.println("Hello from world");
         System.out.prinln("Just Added the feature of getting the data");
+        System.out.println("Adding the second feature");
     }
 }
